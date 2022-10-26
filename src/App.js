4af5a2ae39e2024e6e38567feb74b5394097f9c6
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-10-26 01:01:17 -0400

    Nav & Hero section have their HTML, Projects sections has it's first project. No CSS has been added excluding to .Section and the page container.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Navi from "./components/Navi/Navi";
+import Projects from "./components/Projects/Projects";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <Navi />
+        <Hero />
+        <Projects />
+      </div>
     </div>
   );
 }
