3c05fd160e55c3e7d3ea1efe6e2a74aa94df9336
murraydeam <deandremurray22@gmail.com>
2024-03-25 09:38:57 -0400

    Footer spelling correction

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className="section contact-section" id="contact">
       <div className="contact-header">
-        <h2>Let'Chat more!</h2>
+        <h2>Let's Chat!</h2>
       </div>
       <a href="mailto:deandremurray22@gmail.com">
         <button className="header-button">Connect</button>
