eb40b18132e419337740e575fa8a9495109799d3
Murray <deandre.murray@purina.nestle.com>
2026-08-05 13:34:49 -0400

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Hero from "./components/Hero/Hero";
 import Navi from "./components/Navi/Navi";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <div className="container">
         <Navi />
         <Hero />
